200d8e89d9b773aa72504e33b8d9dc4d21e71e71
Lasy <1255352506@qq.com>
2022-12-18 17:02:19 +0800

    play

--- app/player/src/App.vue
+++ app/player/src/App.vue
@@ -1,19 +1,7 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
-import { MusicService } from "music-service";
-const service = new MusicService();
-const getPlayList = async (params: any) => {
-  const res = await service.playlist();
-  console.log("res", res);
-};
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="container">
-    <router-link to="/">home</router-link>
-    <router-link to="/test">test</router-link>
     <router-view />
   </div>
 </template>
