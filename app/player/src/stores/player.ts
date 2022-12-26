4a0107d557c7db9576a3909c268c6014653fe9e6
Lasy <1255352506@qq.com>
2022-12-26 16:28:18 +0800

    音乐控制

--- app/player/src/stores/player.ts
+++ app/player/src/stores/player.ts
@@ -1,9 +1,12 @@
 import { MusicService } from "music-service";
 import { defineStore } from "pinia";
 import { useCollectionStore, Collection } from "./collection";
-
+import { Howl, Howler } from "howler";
+import { SongDetail } from "music-service/types/listen-url";
 interface Song {
   url: string;
+  songName: string;
+  raw: SongDetail;
 }
 enum PlayStatus {
   Stop,
@@ -15,8 +18,10 @@ const service = new MusicService();
 interface State {
   song: Song | null;
   status: PlayStatus;
+  progress: number;
   volume: number;
   collection: Collection | null;
+  sound: Howl | null;
 }
 
 export const usePlayerStore = defineStore({
@@ -24,8 +29,10 @@ export const usePlayerStore = defineStore({
   state: (): State => ({
     song: null,
     status: PlayStatus.Stop,
-    volume: 50,
+    progress: 0,
+    volume: 0.5,
     collection: null,
+    sound: null,
   }),
   actions: {
     async prepare(songId: string, collectionId?: number) {
@@ -58,19 +65,132 @@ export const usePlayerStore = defineStore({
         toneFlag: audioFormats.slice(-1)[0].formatType,
       });
       console.log("song detail", res);
-      this.song = res;
+      this.song = {
+        raw: res,
+        songName: res.song.songName,
+        url: res.url,
+      };
+      this.sound = new Howl({
+        src: res.url,
+        html5: true,
+        volume: this.volume,
+        onend: () => {
+          this.next();
+        },
+      });
       return res;
     },
     async play() {
+      if (!this.sound) {
+        return;
+      }
+      this.sound.play();
       console.log("play", this.song);
       this.status = PlayStatus.Playing;
       return this.song;
+    },
+
+    async pause() {
+      if (this.status !== PlayStatus.Playing) {
+        return;
+      }
+      this.sound?.pause();
+      this.status = PlayStatus.Paused;
+    },
+    async resume() {
+      if (this.status !== PlayStatus.Paused) {
+        return;
+      }
+      this.sound?.play();
+      this.status = PlayStatus.Playing;
+    },
+
+    async switch() {
+      if (!this.sound) {
+        return;
+      }
+      switch (this.status) {
+        case PlayStatus.Stop:
+          return this.play();
+
+        case PlayStatus.Playing:
+          return this.pause();
+
+        case PlayStatus.Paused:
+          return this.resume();
+
+        default:
+          break;
+      }
+    },
+
+    async stop() {
+      if (!this.sound) {
+        return;
+      }
+      console.log(`stop`, this.song);
+      this.sound.stop();
+      this.sound = null;
+      this.song = null;
+      this.progress = 0;
+    },
+
+    async seek(progress: number) {
+      if (progress > 1) {
+        progress = 1;
+      }
+      if (progress < 0) {
+        progress = 0;
+      }
+      if (!this.sound) {
+        return;
+      }
+      const duration = this.sound.duration();
+      this.sound.seek(duration * progress);
+      this.progress = progress;
+    },
+    async setVolume(volume: number) {
+      this.volume = volume;
+      if (!this.sound) {
+        return;
+      }
+      this.sound.volume(volume);
+    },
+
+    getRandomSong() {
+      if (!this.collection?.list.length) {
+        return;
+      }
+      const song =
+        this.collection.list[
+          Math.floor(Math.random() * this.collection.list.length)
+        ];
+      return song;
+    },
+
+    async next() {
+      const song = this.getRandomSong();
+      if (!song) {
+        return;
+      }
+      await this.stop();
+      await this.prepare(song.songId);
+      await this.play();
+      return song;
     },
   },
   getters: {
     url(state) {
       console.log("getter");
       return state.song?.url ?? "";
+    },
+    statusText(state) {
+      const textMap: Record<PlayStatus, string> = {
+        [PlayStatus.Paused]: "已暂停",
+        [PlayStatus.Playing]: "播放中",
+        [PlayStatus.Stop]: "已结束",
+      };
+      return textMap[state.status];
     },
   },
 });
