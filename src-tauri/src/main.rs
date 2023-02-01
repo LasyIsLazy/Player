ae2dc41db3f36e407c58036c5c90415062706ff9
Lasy <1255352506@qq.com>
2023-02-01 15:38:03 +0800

    debug优化

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,6 +11,15 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = tauri::Manager::get_window(app, "main").unwrap();
+                window.open_devtools();
+                // window.close_devtools();
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
